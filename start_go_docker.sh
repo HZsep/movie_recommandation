163c4c2af2a6d36c737c7b12ec7afc28f220a80e
HZsep <zhaofeiyun2000929@163.com>
2024-02-19 23:18:00 -0500

    Link File system into container

--- start_go_docker.sh
+++ start_go_docker.sh
@@ -1,4 +1,4 @@
- docker run -t -i --rm -v /Users/zfy/recommandation/:/go  golang:1.22rc2-bookworm bash
-
+FILEPATH=$(dirname "$0")
+docker run -t -i --rm -v $FILEPATH:/go golang:1.22rc2-bookworm bash
 
 
